53ded7e55aa2828ec2daedfe3b4b93a1cdd07d7a
iortegaat <iortegamancera@gmail.com>
2019-05-25 12:06:55 +0200

    feat(component): Search input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/home/home.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    SearchInputComponent
   ],
   imports: [
     BrowserModule,
