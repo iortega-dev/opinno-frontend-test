68df84659db8f42f4932ada609c184865f647598
iortegaat <iortegamancera@gmail.com>
2019-05-26 03:57:00 +0200

    feat(recordlist): implemented record service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { HistoricalService } from './services/historical.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'OpinnoSWAPI';
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private historicalSrv: HistoricalService) {
+
+    // Retrieves historical first time
+    this.historicalSrv.getHistorical();
+
+    this.router.events.subscribe((event: Event) => {
+      // When navigation ends
+      if (event instanceof NavigationEnd) {
+        let routeTitle;
+
+        /**
+         *  IF movie link
+         *  ELSE page link
+         */
+        if (this.router.getCurrentNavigation().extras.state) {
+          routeTitle = this.router.getCurrentNavigation().extras.state.title;
+        } else if (this.activatedRoute.snapshot.firstChild.data.title) {
+          routeTitle = this.activatedRoute.snapshot.firstChild.data.title;
+        }
+
+        // Set title if defined
+        if (routeTitle) {
+          this.historicalSrv.addRecord(routeTitle, event.urlAfterRedirects);
+        }
+      }
+    });
+  }
+
+
 }
---
+++ src/app/services/historical.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Record, RecordImpl } from '../models/Record';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HistoricalService {
+
+  records: Record[];
+
+  constructor() { }
+
+  /**
+   * Tries to recover the historical data from localStorage, if it's empty, it initialise it
+   * @returns Record[] historical data
+   */
+  getHistorical() {
+    if (localStorage.getItem('visit_records')) {
+        this.records = JSON.parse(localStorage.getItem('visit_records'));
+    } else {
+        this.records = [];
+    }
+    return this.records;
+  }
+
+  /**
+   * Adds a record to the historical if the last item is not the same
+   * @param record Record to save
+   */
+  addRecord(title, url) {
+      const record = new RecordImpl(title, url);
+      if (Array.isArray(this.records) && this.records.length) {
+        if (this.records[this.records.length - 1].url !== record.url) {
+          this.records.push(record);
+          localStorage.setItem('visit_records', JSON.stringify(this.records));
+        }
+      } else {
+        this.records.push(record);
+        localStorage.setItem('visit_records', JSON.stringify(this.records));
+      }
+  }
+
+}
