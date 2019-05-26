c38a97b90b6b7d2ec1db473eec0a01effe908854
iortegaat <iortegamancera@gmail.com>
2019-05-26 12:24:42 +0200

    feat(historical): New historical logic

--- src/app/services/historical.service.ts
+++ src/app/services/historical.service.ts
@@ -30,14 +30,25 @@ export class HistoricalService {
   addRecord(title, url) {
       const record: Record = { title, url };
       if (Array.isArray(this.records) && this.records.length) {
-        if (this.records[this.records.length - 1].url !== record.url) {
+          this.removeRecord(record);
           this.records.push(record);
           localStorage.setItem('visit_records', JSON.stringify(this.records));
-        }
       } else {
         this.records.push(record);
         localStorage.setItem('visit_records', JSON.stringify(this.records));
       }
+  }
+
+  /**
+   * Removes the record coincidence from records
+   * @param rec Record to remove
+   */
+  removeRecord(rec: Record) {
+    this.records.forEach( (item, index) => {
+      if (rec.title === item.title && rec.url === item.url) {
+        this.records.splice(index, 1);
+      }
+    });
   }
 
 }
